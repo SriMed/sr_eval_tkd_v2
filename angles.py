ae621f8198f9446ef03db0148000db791770a459
Srilakshmi Medarametla <bawesomesri2@outlook.com>
2020-10-15 14:22:01 -0400

    more angles dev

--- angles.py
+++ angles.py
@@ -4,6 +4,8 @@ import numpy as np
 import math
 import json
 
+body_25_num_joints = 25
+
 bt_joints = [
     [8,1,0],
     [2,1,5],
@@ -22,18 +24,25 @@ bt_joints = [
 ]
 #note that the returned angle will be in radians
 #where coordinates is a numpy array
-def calculate_angles(joints, coordinates):
-    for j in joints:
+def calculate_angles(angle_jgroup, coordinates):
+    angles = []
+    for j in angle_jgroup:
         s,c,e = j
-        s =
-        angle = math.acos()
+        s_hat = coordinates[s] - coordinates[c]
+        c_hat = coordinates[c] - coordinates[c]
+        e_hat = coordinates[e] - coordinates[c]
+        alpha = math.acos((s_hat*e_hat)/(np.linalg.norm(s_hat)*np.linalg.norm(e_hat)))
+        angles.append(alpha)
+    return angles
 
 def testing(ij):
     with open(ij) as json_file:
         frame = json.load(json_file)
 
-    coordintes = frame['pose_keypoints_2d']
-    calculate_angles(bt_joints, )
+    coord = frame['people'][0]['pose_keypoints_2d']
+    coord = {(k/3):np.array([coord[k:k+2]]) for k in range(0, len(coord),3)}
+
+    calculate_angles(bt_joints, coord)
 
 
-calculate_angles()
+testing('sample_output.json')
