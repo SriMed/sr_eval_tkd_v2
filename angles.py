a66efc02355e360eefccd664f2a31f75ad1f2a9f
Srilakshmi Medarametla <bawesomesri2@outlook.com>
2020-10-25 18:07:36 -0400

    final angles dev

--- angles.py
+++ angles.py
@@ -1,38 +1,43 @@
 #to calculate angles between joints
 
+
+#note that math returns angles in radians
 import numpy as np
 import math
 import json
 
 body_25_num_joints = 25
 
-bt_joints = [
-    [8,1,0],
-    [2,1,5],
-    [1,2,3],
-    [2,3,4],
-    [1,5,6],
-    [5,6,7],
-    [8,9,10],
-    [9,10,11],
-    [8,12,13],
-    [12,13,14],
-    [4,8,7],
-    [11,8,14],
-    [4,8,1],
-    [1,8,7]
-]
+bt_joints = {
+    0: [8,1,0],
+    1: [2,1,5],
+    2: [1,2,3],
+    3: [2,3,4],
+    4: [1,5,6],
+    5: [5,6,7],
+    6: [8,9,10],
+    7: [9,10,11],
+    8: [8,12,13],
+    9: [12,13,14],
+    10: [4,8,7],
+    11: [11,8,14],
+    12: [4,8,1],
+    13: [1,8,7]
+}
+
 #note that the returned angle will be in radians
 #where coordinates is a numpy array
 def calculate_angles(angle_jgroup, coordinates):
-    angles = []
-    for j in angle_jgroup:
-        s,c,e = j
+    angles = {}
+    for i in angle_jgroup:
+        s,c,e = angle_jgroup[i]
         s_hat = coordinates[s] - coordinates[c]
         c_hat = coordinates[c] - coordinates[c]
         e_hat = coordinates[e] - coordinates[c]
-        alpha = math.acos((s_hat*e_hat)/(np.linalg.norm(s_hat)*np.linalg.norm(e_hat)))
-        angles.append(alpha)
+
+        dot_product = sum(s_hat[0][i]*e_hat[0][i] for i in range(len(s_hat[0])))
+        alpha = math.acos((dot_product)/(np.linalg.norm(s_hat)*np.linalg.norm(e_hat)))
+        angles[i] = alpha
     return angles
 
 def testing(ij):
@@ -42,7 +47,8 @@ def testing(ij):
     coord = frame['people'][0]['pose_keypoints_2d']
     coord = {(k/3):np.array([coord[k:k+2]]) for k in range(0, len(coord),3)}
 
-    calculate_angles(bt_joints, coord)
+    ang = calculate_angles(bt_joints, coord)
+    print(ang)
 
 
 testing('sample_output.json')
