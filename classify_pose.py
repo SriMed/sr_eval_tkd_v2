c28400f986bd99f54fe37fa1c5bbc41363eca09b
Srilakshmi Medarametla <bawesomesri2@outlook.com>
2021-04-13 14:21:02 -0400

    KNN pipeline v2 + DTW updates

--- classify_pose.py
+++ classify_pose.py
@@ -7,43 +7,59 @@ from gen_kmeans import get_cluster_centroids
 
 # identifiers = pickle.load(open('xy_Static/identifiers2move.p', 'rb'))
 
-def get_data(path_to_ks, suffix):
+hand_vp = 20
+kick_vp = 24
+
+hand_techniques = ["High block", "Middle block (in-to-out)", "Middle block (out-to-in)", "Knife hand block", "Low block", "Punch (middle-level)", "High punch (face-level)"]
+kicking_techniques = ["Front kick", "Round kick", "Side kick"]
+
+lu = 'xy_Static'
+
+def get_data(path_to_ks, suffix, new_identifiers=False):
     ks = pickle.load(open(path_to_ks, 'rb'))
 
     num_angles = 14
-    total_poses = (5 * 2 * 7) + (6 * 2 * 3)
+    total_poses = (hand_vp * 2 * len(hand_techniques)) + (kick_vp * 2 * len(kicking_techniques))
 
     X = np.empty((total_poses, num_angles))
     y = np.array([])
 
-    identifier2move = {}
-
-    identifiers = [i for i in range(len(ks.values()))]
-    random.shuffle(identifiers)
+    if new_identifiers == True:
+        identifier2move = {}
+        move2identifier = {}
+        identifiers = [i for i in range(len(ks.values()))]
+        random.shuffle(identifiers)
+    else:
+        identifier2move, move2identifier = pickle.load(open(os.path.join(lu, f'moveids.p'), 'rb'))
 
     count = 0
     for move in ks:
-        moveid = identifiers.pop()
-        identifier2move[moveid] = move
+        if new_identifiers == True:
+            moveid = identifiers.pop()
+            identifier2move[moveid] = move
+            move2identifier[move] = moveid
+        else:
+            moveid = move2identifier[move]
         vocab_poses = ks[move].cluster_centers_
         for pose in vocab_poses:
             X[count] = pose #where X is the collection of vocab poses that map
             y = np.append(y, [moveid])
             count += 1
-    np.savez(f'xy_Static/{suffix}', X, y)
-    pickle.dump(identifier2move, open('xy_Static/identifiers2move.p', 'wb'))
-    return X,y, identifier2move
+    np.savez(os.path.join(lu, suffix), X, y)
+    if new_identifiers == True:
+        pickle.dump((identifier2move, move2identifier), open(os.path.join(lu, f'moveids.p'), 'wb'))
+    return X,y, identifier2move, move2identifier
 
-def classify_videos(ips, tech):
-    path_to_ks = 'xy_Static/ks-h5k6.p'
+def classify_videos(ips, tech, ks_name, new_ids=False):
+    path_to_ks = os.path.join(lu, ks_name)
     suffix = path_to_ks[path_to_ks.find('-')+1:path_to_ks.find('.p')]
 
-    if not os.path.exists(f'xy_Static/{suffix}.npz'):
-        X, y, identifier2move = get_data(path_to_ks, suffix)
+    if not os.path.exists(os.path.join(lu, f'{suffix}.npz')):
+        X, y, identifier2move, move2identifier = get_data(path_to_ks, suffix, new_identifiers=new_ids)
     else:
-        npzfile = np.load(f'xy_Static/{suffix}.npz')
+        npzfile = np.load(os.path.join(lu, f'{suffix}.npz'))
         X,y = npzfile['arr_0'], npzfile['arr_1']
-        identifier2move = pickle.load(open('xy_Static/identifiers2move.p', 'rb'))
+        identifier2move, move2identifier = pickle.load(open(os.path.join(lu, f'moveids.p'), 'rb'))
 
     # neigh = KNeighborsClassifier(n_neighbors=5)
     neigh = KNeighborsClassifier(n_neighbors=10)
@@ -54,11 +70,11 @@ def classify_videos(ips, tech):
     for i, ip in enumerate(ips):
 
         if tech[i] == 'h':
-            vp = int(suffix[suffix.find('h') + 1:suffix.find('h') + 2])
+            num_vp = int(suffix[suffix.find('h')+1:suffix.find('k')])
         else:
-            vp = int(suffix[suffix.find('k') + 1:suffix.find('k') + 2])
+            num_vp = int(suffix[suffix.find('k')+1:])
 
-        sample_kmeans = get_cluster_centroids(vp, [ip])
+        sample_kmeans = get_cluster_centroids(num_vp, [ip])
 
         res = []
         for pose in sample_kmeans.cluster_centers_:
@@ -72,8 +88,8 @@ def classify_videos(ips, tech):
             else:
                 tally[m] += 1
 
-        for w in sorted(tally, key=tally.get, reverse=True):
-            print(w, tally[w])
+        # for w in sorted(tally, key=tally.get, reverse=True):
+        #     print(w, tally[w])
             # final_res.append(tally.values())
         maxvote_move = max(tally, key=tally.get)
         final_res.append((maxvote_move, f'{tally[maxvote_move]}/{sum(tally.values())}'))
@@ -85,5 +101,5 @@ def print_res(ips, final_res):
 
 ips = ['xy_Static/IMG_5002_si.npy', 'xy_Static/fk_rd_0_si.npy', 'xy_Static/fk_rd_1_si.npy']
 tech = ['h', 'k', 'k']
-res = classify_videos(ips, tech)
+res = classify_videos(ips, tech, f'ks-h{hand_vp}k{kick_vp}.p', new_ids=True)
 print_res(ips, res)
